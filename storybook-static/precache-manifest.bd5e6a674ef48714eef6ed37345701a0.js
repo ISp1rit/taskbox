c5fd573b0124bc2b7d718272e8f84fd148e33abf
Yaroslav Moiseev <yaroslavmo@wizardsdev.com>
2019-06-13 00:21:56 +0300

    taskbox UI

---
+++ storybook-static/precache-manifest.bd5e6a674ef48714eef6ed37345701a0.js
@@ -0,0 +1,19 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cd2a294d592434b928cb1914bbda95fe",
+    "url": "iframe.html"
+  },
+  {
+    "url": "main.2fb598aa3774eb017da7.bundle.js"
+  },
+  {
+    "revision": "0d28337109d72a1fa298",
+    "url": "main.css"
+  },
+  {
+    "url": "runtime~main.2fb598aa3774eb017da7.bundle.js"
+  },
+  {
+    "url": "vendors~main.2fb598aa3774eb017da7.bundle.js"
+  }
+]);
